96a48efcd8ce112844c9a8be0d3d339daf0103e3
amusingimpala75 <69653100+amusingimpala75@users.noreply.github.com>
2025-05-20 21:46:46 -0700

    finished haskell solution

--- haskell/main.hs
+++ haskell/main.hs
@@ -1,6 +1,7 @@
 import Data.Char
 import Data.List
 import Data.Bifunctor
+import Data.Ord
 
 data DictEntry = Terminal [(Char, DictEntry)]
                | Nonterminal [(Char, DictEntry)]
@@ -38,17 +39,65 @@ getDict :: [String] -> Dict
 getDict words = Nonterminal $ map (second makeDictEntry . tuplifyWords) $ groupByFirstChar words
 
 dictEntryContains :: DictEntry -> Bool -> String -> Bool
-dictEntryContains (Terminal children) _ "" = True
+dictEntryContains (Terminal _) _ "" = True
 dictEntryContains (Nonterminal _) partial "" = partial
 dictEntryContains (Terminal children) partial word = dictMapContains children partial word
 dictEntryContains (Nonterminal children) partial word = dictMapContains children partial word
 
 dictMapContains :: [(Char, DictEntry)] -> Bool -> String -> Bool
-dictMapContains children partial (x:xs) = let results = filter (\(char, entries) -> char == x) children in not (null results) && dictEntryContains (snd $ head results) partial xs
+dictMapContains children partial (x:xs) = let results = filter (\(char, _) -> char == x) children in not (null results) && dictEntryContains (snd $ head results) partial xs
+
+getConnected :: (Int, Int) -> [(Int, Int)]
+getConnected (x, y) = filterBounds [(x + dx, y + dy) | dx <- [-1..1], dy <- [-1..1]]
+
+filterBounds :: [(Int, Int)] -> [(Int, Int)]
+filterBounds = filter (\(x, y) -> x `elem` [0..5-1] && y `elem` [0..5-1])
+
+mapChar :: Char -> String
+mapChar '1' = "AN"
+mapChar '2' = "ER"
+mapChar '3' = "HE"
+mapChar '4' = "IN"
+mapChar '5' = "QU"
+mapChar '6' = "TH"
+mapChar x = [x]
+
+getCandidate :: Board -> [(Int, Int)] -> String
+getCandidate (Board chars) = concatMap (\(x, y) -> mapChar $ chars!!y!!x)
+
+getCandidates :: Dict -> Board -> [(Int, Int)] -> [String]
+getCandidates dict board positions@(_:_)
+  | not $ dictEntryContains dict True candidate = []
+  | otherwise = candidate:concatMap (getCandidates dict board . (:positions)) neighbors
+  where neighbors = filter (not . flip elem positions) $ getConnected $ head positions
+        candidate = getCandidate board $ reverse positions
+
+getWords :: Dict -> Board -> [String]
+getWords dict board = concatMap (getCandidates dict board) [[(x, y)] | x <- [0..5-1], y <- [0..5-1]]
+
+customSort :: String -> String -> Ordering
+customSort "" "" = EQ
+customSort (_:_) "" = GT
+customSort "" (_:_) = LT
+customSort a@(x:xs) b@(y:ys) = let cmp = comparing length a b in if cmp /= EQ then cmp else if x /= y then compare x y else customSort xs ys
+
+scoreWord :: Int -> Int
+scoreWord 4 = 1
+scoreWord 5 = 2
+scoreWord 6 = 3
+scoreWord 7 = 5
+scoreWord x
+  | x >= 8 = 11
+  | otherwise = 0
 
 main = do
   board <- getBoard
-  print board
-
+  
   dict <- getDict <$> readDict "./dictionary.txt"
-  print dict
+
+  let words = sortBy customSort $ filter (dictEntryContains dict False) $ filter (\word -> length word >= 4) $ nub $ getWords dict board
+      count = length words
+      score = sum $ map (scoreWord . length) words in do
+    putStr $ unlines words
+    putStrLn $ "Word count: " ++ show count
+    putStrLn $ "Score: " ++ show score
